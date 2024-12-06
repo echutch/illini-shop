7bee96bd0814d0d202db93da394cc601ceb5a961
echutch <echutch429@gmail.com>
2024-12-06 02:33:14 -0600

    Cleanup

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6gy0q+m6dn=xj1paua!0pm4b+tgv%je&us#pb2fwlz9e9c7cu('
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
