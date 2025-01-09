ce14ace8b1dcda6f8141a263942a0e4f661e07dd
echutch <echutch429@gmail.com>
2025-01-09 17:35:15 -0600

    Created models: product, cart item, order

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    description = models.TextField()
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
--- backend/src/settings.py
+++ backend/src/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'src.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),  # The name of your database
-        'USER': os.getenv('DB_USER'),  # The user for the database
-        'PASSWORD': os.getenv('DB_PASSWORD'),  # The password for the database
-        'HOST': 'db',  # The hostname of the database, usually 'db' in Docker 
-        'PORT': '5432',  # The port for PostgreSQL
+        'NAME': os.getenv('DB_NAME'),  
+        'USER': os.getenv('DB_USER'),  
+        'PASSWORD': os.getenv('DB_PASSWORD'),  
+        'HOST': os.getenv('DB_HOST'),   
+        'PORT': os.getenv('DB_PORT'),  
     }
 }
 
